3892f79d898617afae748bdec969c2dfdc9b7716
Yao ZHANG <yao.zhang@greenwaves-technologies.com>
2018-03-23 11:13:14 +0100

    updated rt_conf.c

--- pulp-rt/rt_conf.c
+++ pulp-rt/rt_conf.c
@@ -2,6 +2,8 @@
 
 rt_dev_t __rt_devices[] = {
   {"camera", 0x509, (void *)&himax_desc, {{}}},
+  {"microphone0", 0x8, (void *)&i2s_desc, {{}}},
+  {"microphone1", 0x18, (void *)&i2s_desc, {{}}},
 };
 
 int __rt_nb_devices = 1;
