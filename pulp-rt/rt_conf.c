f32e80613be61cf8e9191d85e767389f8264b01c
Yao ZHANG <yao.zhang@greenwaves-technologies.com>
2018-03-29 10:08:35 +0200

    added 2 more periphs in conf.c

--- pulp-rt/rt_conf.c
+++ pulp-rt/rt_conf.c
@@ -5,6 +5,8 @@ rt_dev_t __rt_devices[] = {
   {"microphone", 0x8, (void *)&i2s_desc, {{}}},
   {"microphone0", 0x8, (void *)&i2s_desc, {{}}},
   {"microphone1", 0x18, (void *)&i2s_desc, {{}}},
+  {"hyperflash", 0x3, (void *)&hyperflash_desc, {{8388608}}},
+  {"hyperram", 0x3, (void *)NULL, {{8388608}}},
 };
 
-int __rt_nb_devices = 4;
+int __rt_nb_devices = 6;
