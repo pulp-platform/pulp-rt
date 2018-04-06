d4be8769f2219be054ca0506a810446786a79beb
Francesco Paci <francesco.paci@greenwaves-technologies.com>
2018-04-06 12:12:25 +0200

    Modified himax driver, changed some register, removed anti flicker

--- pulp-rt/drivers/camera/himax.c
+++ pulp-rt/drivers/camera/himax.c
@@ -37,7 +37,7 @@
 
 #include "rt/rt_api.h"
 
-//#define ACTIVATE_I2C 1
+#define ACTIVATE_I2C 1
 
 //TODO: this sequence could be optimized
 static himax_reg_cfg_t himaxRegInit[] = {
@@ -60,11 +60,15 @@ static himax_reg_cfg_t himaxRegInit[] = {
     {0x3059, 0x1E},
     {0x3064, 0x00},
     {0x3065, 0x04},             //  pad pull 0
+    
     {BLC_CFG, 0x43},            //  BLC_on, IIR
+
     {0x1001, 0x43},             //  BLC dithering en
     {0x1002, 0x43},             //  blc_darkpixel_thd
-    {0x0350, 0x7F},             //  Dgain Control
+    {0x0350, 0x00},             //  Dgain Control
     {BLI_EN, 0x01},             //  BLI enable
+    {0x1003, 0x00},             //  BLI Target [Def: 0x20]
+    
     {DPC_CTRL, 0x01},           //  DPC option 0: DPC off   1 : mono   3 : bayer1   5 : bayer2
     {0x1009, 0xA0},             //  cluster hot pixel th
     {0x100A, 0x60},             //  cluster cold pixel th
@@ -84,17 +88,34 @@ static himax_reg_cfg_t himaxRegInit[] = {
     {0x2014, 0x58},
     {0x2017, 0x00},
     {0x2018, 0x9B},
-    {0x2100, 0x01},
-    {0x2104, 0x07},
-    {0x2105, 0x02},
-    {0x2106, 0x14},
+
+    {0x2100, 0x01},     //Automatic Exposure Gain Control
+    {0x2101, 0xB0},     //AE target mean [Def: 0x3C]
+    {0x2102, 0x0A},     //AE target mean [Def: 0x0A]
+    
+    {0x210D, 0x10},     //Damping Factor [Def: 0x20]
+    
+    {0x0205, 0x00},     //Analog Global Gain
+    {0x020E, 0x50},     //Digital Gain High
+    {0x020F, 0x00},     //Digital Gain Low
+    {0x0202, 0x03},     //Integration H [Def: 0x01]
+    {0x0203, 0x08},     //Integration L [Def: 0x08]
+
+    {0x2104, 0x05},
+    {0x2105, 0x01},
+    
+    {0x2106, 0x54},
+
     {0x2108, 0x03},
-    {0x2109, 0x03},
-    {0x210B, 0x80},
+    {0x2109, 0x04},
+    
+    {0x210B, 0xC0},
+    {0x210E, 0x00}, //Flicker Control KEEP IT AT 0!!!!!!!!!!!!!!!!
     {0x210F, 0x00},
-    {0x2110, 0x85},
+    {0x2110, 0x3C},
     {0x2111, 0x00},
-    {0x2112, 0xA0},
+    {0x2112, 0x32},
+
     {0x2150, 0x30},
     {0x0340, 0x02},
     {0x0341, 0x16},
@@ -107,8 +128,8 @@ static himax_reg_cfg_t himaxRegInit[] = {
     {0x3011, 0x70},
     {0x3059, 0x02},
     {0x3060, 0x01},
-    {IMG_ORIENTATION, 0x03}, // change the orientation
-    {0x0104, 0x01},
+    {IMG_ORIENTATION, 0x01}, // change the orientation
+    {0x0104, 0x01}
 };
 
 RT_L2_DATA unsigned char valRegHimax;
@@ -160,8 +181,10 @@ static void _himaxWakeUP (rt_camera_t *cam){
 
 void _himaxReset(rt_camera_t *cam){
     himaxRegWrite(cam, SW_RESET, HIMAX_RESET);
-    while (himaxRegRead(cam, MODE_SELECT) != HIMAX_Standby)
+    while (himaxRegRead(cam, MODE_SELECT) != HIMAX_Standby){
         himaxRegWrite(cam, SW_RESET, HIMAX_RESET);
+        rt_time_wait_us(50);
+    }
 }
 
 void _himaxStandby(rt_camera_t *cam){
