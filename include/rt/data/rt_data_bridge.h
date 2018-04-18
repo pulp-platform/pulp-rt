704d55fcc61d2ba4d4c9c2098463c538f198fdf9
Germain Haugou <germain.haugou@iie.ee.ethz.ch>
2018-04-18 10:50:51 +0200

    Added new API to interact with the bridge

--- include/rt/data/rt_data_bridge.h
+++ include/rt/data/rt_data_bridge.h
@@ -30,48 +30,24 @@
  * limitations under the License.
  */
 
-#ifndef __RT_RT_API_H__
-#define __RT_RT_API_H__
+#ifndef __RT_RT_DATA_BRIDGE_H__
+#define __RT_RT_DATA_BRIDGE_H__
 
-#include "rt/rt_data.h"
-#include "rt/rt_utils.h"
-#include "rt/rt_extern_alloc.h"
-#include "rt/rt_thread.h"
-#include "rt/rt_event.h"
-#include "rt/rt_flash.h"
-#include "rt/rt_dev.h"
-#include "rt/rt_periph.h"
-#include "rt/rt_irq.h"
-#include "rt/rt_cluster.h"
-#include "rt/rt_hyper.h"
-#include "rt/rt_alloc.h"
-#include "rt/rt_debug.h"
-#include "rt/rt_config.h"
-#include "rt/rt_pe.h"
-#include "rt/rt_i2c.h"
-#include "rt/rt_camera.h"
-#include "rt/rt_himax.h"
-#include "rt/rt_ov7670.h"
-#include "rt/rt_dma.h"
-#include "rt/rt_sync_mc.h"
-#include "rt/rt_perf.h"
-#include "rt/rt_time.h"
-#include "rt/rt_freq.h"
-#include "rt/rt_i2s.h"
-#include "rt/rt_fs.h"
-#include "rt/rt_error.h"
-#if defined(ARCHI_UDMA_HAS_UART) && UDMA_VERSION >= 2 || defined(ARCHI_HAS_UART)
-#include "rt/rt_uart.h"
+/// @cond IMPLEM
+
+#ifndef LANGUAGE_ASSEMBLY
+
+#include <stdint.h>
+
+#include "hal/debug_bridge/debug_bridge.h"
+
+typedef struct rt_bridge_req_s {
+  hal_bridge_req_t header;
+  rt_event_t *event;
+} rt_bridge_req_t;
+
 #endif
-#include "rt/rt_spim.h"
-#include "rt/rt_rtc.h"
-//#if defined(APB_SOC_VERSION) && APB_SOC_VERSION == 1
-#include "rt/rt_pad.h"
-//#endif
-#ifdef GPIO_VERSION
-#include "rt/rt_gpio.h"
-#endif
-#include "rt/rt_voltage.h"
-#include "rt/rt_bridge.h"
+
+/// @endcond
 
 #endif
