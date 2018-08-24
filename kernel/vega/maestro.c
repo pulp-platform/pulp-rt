2b92ff445a49daa413c26069f5a8b85723685f9f
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2018-08-24 18:44:52 +0200

    Ported to new vega pmu

--- kernel/vega/maestro.c
+++ kernel/vega/maestro.c
@@ -22,33 +22,6 @@
 #include "rt/rt_api.h"
 #include "stdio.h"
 
-#define PMU_DLC_PCTRL_REG ARCHI_PMU_ADDR + 0x00
-#define PMU_DLC_IFR_REG   ARCHI_PMU_ADDR + 0x10
-
-#define PMU_STATE_SOC_NV     2
-#define PMU_STATE_SOC_LV     3
-#define PMU_STATE_SOC_CLU_NV 4
-#define PMU_STATE_SOC_CLU_LV 5
-#define PMU_STATE_DEEP_SLEEP 0
-
-#define PMU_PICL_WIU  1
-#define PMU_PICL_ICU0 2
-#define PMU_PICL_ICU1 3
-#define PMU_PICL_ICU2 4
-
-#define PMU_WIU_ISPMR_0 0
-#define PMU_WIU_ISPMR_1 1
-#define PMU_WIU_IFR_0   2
-#define PMU_WIU_IFR_1   3
-
-
-void set_PMUState(unsigned int state) {
-  pulp_write32(PMU_DLC_IFR_REG,0xFF); //clears previous interrupts
-  unsigned int SetSCUInt = ((1<<state)<<16)|((PMU_PICL_WIU)<<6)|((PMU_WIU_IFR_1)<<1)|1;
-  pulp_write32(PMU_DLC_PCTRL_REG,SetSCUInt);
-  __rt_periph_wait_event(ARCHI_SOC_EVENT_SCU_OK, 1);
-}
-
 
 void __rt_pmu_cluster_power_down()
 {
@@ -85,18 +58,13 @@ void __rt_pmu_cluster_power_up()
 {
   //plp_trace(RT_TRACE_PMU, "Cluster power up\n");
 
-  /* Turn on power, this will also clock ungate the cluster */
-  set_PMUState(PMU_STATE_SOC_CLU_NV);
-
-  // Unblock transactions from dc fifos to soc
-  apb_soc_cluster_isolate_set(0);
+  hal_pmu_state_set(ARCHI_PMU_STATE_SOC_NV_CLU_NV);
 
   // Tell external loader (such as gdb) that the cluster is on so that it can take it
   // into account
-  hal_pmu_bypass_set( (1<<ARCHI_PMU_BYPASS_ENABLE_BIT) | (1<<ARCHI_PMU_BYPASS_CLUSTER_POWER_BIT) | (1<<ARCHI_PMU_BYPASS_CLUSTER_RESET_BIT) | (1<<ARCHI_PMU_BYPASS_CLUSTER_CLOCK_BIT) | (1 << APB_SOC_BYPASS_USER0_BIT));
+  //hal_pmu_bypass_set( (1<<ARCHI_PMU_BYPASS_ENABLE_BIT) | (1<<ARCHI_PMU_BYPASS_CLUSTER_POWER_BIT) | (1<<ARCHI_PMU_BYPASS_CLUSTER_RESET_BIT) | (1<<ARCHI_PMU_BYPASS_CLUSTER_CLOCK_BIT) | (1 << APB_SOC_BYPASS_USER0_BIT));
 }
 
 void __rt_pmu_init()
 {
-  soc_eu_fcEventMask_setEvent(ARCHI_SOC_EVENT_SCU_OK);
 }
