c9df9e70de2533f37efe1b8dc78d5dc5d2f092ba
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-03-27 06:54:42 +0100

    Fixed regression

--- mk/vega/__flags.mk
+++ mk/vega/__flags.mk
@@ -1,7 +1,7 @@
 PULP_LDFLAGS      += 
 PULP_CFLAGS       += 
 PULP_CL_ARCH_CFLAGS ?=  -march=rv32imcxgap9 -mPE=8 -mFC=1 -D__riscv__
-PULP_CL_CFLAGS    += -Wextra -Wall -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wundef -fdata-sections -ffunction-sections -I/home/haugoug/src/pulp-sdk/pkg/sdk/dev/install/include/io -I/home/haugoug/src/pulp-sdk/pkg/sdk/dev/install/include -include /home/haugoug/src/pulp-sdk/build/sdk/pulp-rt/vega/cl_config.h
+PULP_CL_CFLAGS    += -Wextra -Wall -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wundef -fdata-sections -ffunction-sections -I$(TARGET_INSTALL_DIR)/include/io -I$(TARGET_INSTALL_DIR)/include -include $(CURDIR)/mk/vega/fc_config.h
 PULP_CL_OMP_CFLAGS    += -fopenmp -mnativeomp
 ifdef PULP_RISCV_GCC_TOOLCHAIN
 PULP_CL_CC = $(PULP_RISCV_GCC_TOOLCHAIN)/bin/riscv32-unknown-elf-gcc 
@@ -21,7 +21,7 @@ endif
 PULP_ARCH_CL_OBJDFLAGS ?= -Mmarch=rv32imcxgap9
 PULP_ARCH_OBJDFLAGS ?= -Mmarch=rv32imcxgap9
 PULP_FC_ARCH_CFLAGS ?=  -march=rv32imcxgap9 -mPE=8 -mFC=1 -D__riscv__
-PULP_FC_CFLAGS    += -Wextra -Wall -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wundef -fdata-sections -ffunction-sections -I/home/haugoug/src/pulp-sdk/pkg/sdk/dev/install/include/io -I/home/haugoug/src/pulp-sdk/pkg/sdk/dev/install/include -include /home/haugoug/src/pulp-sdk/build/sdk/pulp-rt/vega/fc_config.h
+PULP_FC_CFLAGS    += -Wextra -Wall -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wundef -fdata-sections -ffunction-sections -I$(TARGET_INSTALL_DIR)/include/io -I$(TARGET_INSTALL_DIR)/include -include /home/haugoug/src/pulp-sdk/build/sdk/pulp-rt/vega/fc_config.h
 PULP_FC_OMP_CFLAGS    += -fopenmp -mnativeomp
 ifdef PULP_RISCV_GCC_TOOLCHAIN
 PULP_FC_CC = $(PULP_RISCV_GCC_TOOLCHAIN)/bin/riscv32-unknown-elf-gcc 
@@ -37,4 +37,3 @@ PULP_LD ?= $(PULP_RISCV_GCC_TOOLCHAIN_CI)/bin/riscv32-unknown-elf-gcc
 PULP_FC_OBJDUMP ?= $(PULP_RISCV_GCC_TOOLCHAIN_CI)/bin/riscv32-unknown-elf-objdump
 endif
 PULP_ARCH_FC_OBJDFLAGS ?= -Mmarch=rv32imcxgap9
-pulpRunOpt        += --dir=/home/haugoug/src/pulp-sdk/build/sdk/pulp-rt/vega
