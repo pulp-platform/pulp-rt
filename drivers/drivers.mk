413ec8cf6a01785fb30cc6318bcac46d79f32e4a
Yao ZHANG <yao.zhang@greenwaves-technologies.com>
2019-05-24 06:22:50 -0700

    Added PWM drivers

--- drivers/drivers.mk
+++ drivers/drivers.mk
@@ -98,6 +98,14 @@ endif
 endif
 endif
 
+# PWM
+
+ifeq '$(CONFIG_PWM_ENABLED)' '1'
+PULP_LIB_FC_CFLAGS += -DRT_CONFIG_PWM_ENABLED
+PULP_LIB_FC_SRCS_rt += drivers/pwm/pwm.c
+PULP_LIB_FC_ASM_SRCS_rt += drivers/pwm/pwm_asm.S
+endif
+
 
 # GPIO
 
